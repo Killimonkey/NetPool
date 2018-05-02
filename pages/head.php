f08971f6a7f4fa3c1cf0c180c63c40a6c7f1c11e
Killimonkey <spitekillian@gmail.com>
2018-05-02 12:56:29 +0200

    refresh

--- pages/head.php
+++ pages/head.php
@@ -8,7 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
   <!-- css stylesheet -->
-  <link rel="stylesheet" href="../public/css/style.css"  type="text/css">
+  <link rel="stylesheet" href="../public/css/style.css?refresh=0"  type="text/css">
 
   <!-- Bootstrap -->
   <link  rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
