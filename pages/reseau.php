a6fca8cf7029b4caf699362631167460ebcefb41
Killimonkey <spitekillian@gmail.com>
2018-05-02 12:30:03 +0200

    reseau

---
+++ pages/reseau.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <!-- Affichage du head -->
+  <?php include ("head.php"); ?>
+
+  <!-- Affichage du corps -->
+  <body class="body">
+
+    <!-- Affichage de la nav -->
+    <?php include ("nav.php"); ?>
+    <div class="container-fluid">
+      <div class="row">
+
+        <div class="col-sm-12">
+          <!-- titre-->
+          <div class="col-sm-4 titre">Mes Amis</div>
+          <div class="col-sm-4 titre">Mon réseau Professionnel</div>
+          <div class="col-sm-4 titre">Se Connecter</div>
+
+          <!-- corps -->
+          <div class="col-sm-4 corps"></div>
+          <div class="col-sm-4 corps"></div>
+          <div class="col-sm-4 corps"></div>
+
+
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  <!-- Affichage du footer -->
+  <?php include ("footer.php"); ?>
+
+  <!-- https://developer.mozilla.org/fr/docs/Web/Guide/HTML/Formulaires/Validation_donnees_formulaire -->
+</html>
