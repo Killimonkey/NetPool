9cfb3be7403d674d8efce3521e415e169d5e1607
DESKTOP-UTK1V1E\marie <marie.yahiaoui@edu.ece.fr>
2018-05-04 19:35:58 +0200

    reseau_perfect_owyeahhhhhhhh

---
+++ pages/collegue.php
@@ -0,0 +1,21 @@
+<?php
+  session_start();
+  $id = $_GET['data'];
+
+  // Essayer de se connecter à la base de données
+  try
+  {
+    $bdd = new PDO('mysql:host=localhost;dbname=netpool;charset=utf8', 'root', '');
+
+    // Chercher si l'utilisateur existe déjà
+    $requete = $bdd->prepare('INSERT INTO reseau (utilisateur_1,utilisateur_2,check_ami) VALUES (?,?,?)');
+    $requete->execute(array($id,$_SESSION['$id_utilisateur'],"FALSE"));
+
+    $bdd = null;
+  }
+  catch (Exception $e)
+  {
+    die('Erreur : ' . $e->getMessage());
+  }
+  header('Location:reseau.php');
+?>
