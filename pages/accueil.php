1bafd9fcede7b219aa225eb9d538867ae7ba157f
Amandine01 <amandine.ducruet@free.fr>
2018-05-05 22:03:47 +0200

    conflicts réglés

--- pages/accueil.php
+++ pages/accueil.php
@@ -337,24 +337,12 @@
                       echo '
                       </li>';
                     }
-
-<<<<<<< HEAD
-                    echo '
-                    </div>';
-                    if($aime == FALSE) echo '<a href="aime.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-heart-empty coeur_acceuil"></span></button></a>';
-                    else echo '<a href="aime_plus.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-heart coeur_acceuil"></span></button></a>';
-                    echo '
-                    <a  href="commentaire_ecriture.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-edit"></span></button><a>
-                    <button type="button" class="btn btn-primary option_publier col-sm-4" data-toggle="modal" data-target="#modal_partager"><span class="glyphicon glyphicon-share"></span></button>
-                    </li>';
-=======
                     else {
                       echo '
                       <!-- Une publication -->
                       <li class="media cadre_publication">
                       <!-- PP -->
                       <div class="media-left">';
->>>>>>> a87d07c341e9bd2c145e63bf17fca3559a5d669b
 
                       if($profil != "") echo '<img src="../upload/pp/'.$profil.'" class="media-object" style="width:45px">';
                       else echo '<img src="../public/images/pp_template.jpg" class="media-object" style="width:45px">';
@@ -390,7 +378,7 @@
                       if($aime == FALSE) echo '<a href="aime.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-heart-empty coeur_acceuil"></span></button></a>';
                       else echo '<a href="aime_plus.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-heart coeur_acceuil"></span></button></a>';
                       echo '
-                      <button type="button" class="btn btn-primary option_publier col-sm-4" data-toggle="modal" data-target="#modal_commenter"><span class="glyphicon glyphicon-edit"></span></button>
+                      <a  href="commentaire_ecriture.php?data='.$id_publication.'"><button type="button" class="btn btn-primary option_publier col-sm-4"><span class="glyphicon glyphicon-edit"></span></button><a>
                       <a href="../php/partager.php?data='.$id_publication.'&amp;mec='.$utilisateur['id_utilisateur'].'"><button type="button" class="btn btn-primary option_publier col-sm-4" ><span class="glyphicon glyphicon-share"></span></button></a>';
 
                       // Récupérer les commentaires de la publication
@@ -431,40 +419,6 @@
                 ?>
 
         </ul>
-
-<<<<<<< HEAD
-        <!-- Partager -->
-        <div class="modal fade" id="modal_partager" tabindex="-1" role="dialog" aria-labelledby="modal_partager" aria-hidden="true">
-          <div class="modal-dialog" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="modal_partager">Partager la publication</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-
-              <form action="../php/partager.php" method="post">
-
-                <div class="modal-body">
-                  <!-- Commentaire -->
-                  <div class="form-group">
-                  <label for="comment_partage">Description :</label>
-                  <input type="text" name="comment_partage" class="form-control" required>
-                  </div>
-
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                  <input type="submit" value="Partager" class="btn btn-primary"></input>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-=======
->>>>>>> a87d07c341e9bd2c145e63bf17fca3559a5d669b
-
         </div>
 
       </div>
