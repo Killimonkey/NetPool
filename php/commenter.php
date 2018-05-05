6c6d248156af38465e6b2fd5c2bf7e5f93cde8ea
Killimonkey <spitekillian@gmail.com>
2018-05-05 19:50:29 +0200

    commenter une publi

---
+++ php/commenter.php
@@ -0,0 +1,52 @@
+<?php
+
+  session_start();
+  $date = date("d/m/Y");
+  $heure = date("H:i:s");
+  $datetime = strtolower($date.' '.$heure);
+
+  // On recupere les valeurs
+  $commentaire = isset($_POST["commentaire"])?$_POST["commentaire"] : "";
+  $error = "";
+
+  // On teste si les champs ne sont pas vide sinon on affiche un message d'erreur
+  if($commentaire == "")
+  {
+   $error .= "Pas de commentaire <br/>";
+  }
+
+  // Si il n'y a pas d'erreur
+  if ($error != "")
+  {
+    echo "Erreur :<br> $error";
+  }
+  else
+  {
+    // Essayer de se connecter à la base de données
+    try
+    {
+      $bdd = new PDO('mysql:host=localhost;dbname=netpool;charset=utf8', 'root', 'root');
+      // Modifier le comment
+      $requete = $bdd->prepare('INSERT INTO commentaire (description) VALUES (?)');
+      $requete->execute(array($commentaire));
+      $requete = $bdd->prepare('SELECT id_commentaire FROM commentaire ');
+      $requete->execute(array($commentaire));
+      $id_pub = $requete->fetch();
+      $requete = $bdd->prepare('INSERT INTO ecrit (id_utilisateur,id_commentaire) VALUES (?,?)');
+      $requete->execute(array($_SESSION['$id_utilisateur'],$id_pub[0]));
+      $requete = $bdd->prepare('INSERT INTO est_ecrit_dans (id_publication,id_commentaire) VALUES (?,?)');
+      $requete->execute(array($id_publication,$id_pub[0]));
+      // Récupérer le résultat
+      $resultat = $requete->fetch();
+      // Enregistrer dans la variable
+      $_SESSION['$commentaire'] = $commentaire;
+      $bdd = null;
+      header('Location:../pages/accueil.php');
+    }
+    catch (Exception $e)
+    {
+      die('Erreur : ' . $e->getMessage());
+    }
+
+  }
+?>
